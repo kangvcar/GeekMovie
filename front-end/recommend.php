1fbffdb2d7b1943aca84755433006c879f9dc459
kangvcar <kangvcar@126.com>
2018-11-06 11:13:07 +0800

    修改首页<为你推荐>栏目的样式

--- front-end/recommend.php
+++ front-end/recommend.php
@@ -1,22 +1,22 @@
-<!-- 精彩瞬间 start -->
+<!-- 为你推荐 start -->
 <div class="title text-right text-blue" id="recommend">
   <h1><i class="fa fa-instagram fa-1x" aria-hidden="true"></i>为你推荐</h1>
 </div>
 <div class="row">
   <?php 
       $recommendsql = "SELECT mid, mname, mimgurl FROM movie LIMIT 4;";
-      $result2 = $conn->query($dongzuomoviesql);
-      if ($row = $result2->num_rows > 0) {
-        while ($row = $result2->fetch_row()) {
+      $result32 = $conn->query($recommendsql);
+      if ($row = $result32->num_rows > 0) {
+        while ($row = $result32->fetch_row()) {
           print <<<EOY
-              <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
+              <div class="col-md-3 col-sm-3">
+              <div class="img-class">
                   <a href="./details.php?movieid=$row[0]"><img src="$row[2]" alt="$row[1]"></a>
-                </a>
               </div>
+            </div>
 EOY;
         }
       }
    ?>
 </div>
-<!-- 精彩瞬间 end -->
+<!-- 为你推荐 end -->
