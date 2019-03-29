9c7ebf81e120f6d4a05d40095521afb1e637db3e
Kangvcar <kangvcar@126.com>
2019-03-29 21:11:31 +0800

    Add colums role

--- back-end-v5.0/login_admin_process.php
+++ back-end-v5.0/login_admin_process.php
@@ -2,12 +2,19 @@
     include_once("dbconnection.php");
     $userName = addslashes($_POST['userName']);
     $password = addslashes($_POST['password']);
-    $loginSQL = "select * from admin where username='$userName' and password='$password'";
+    $loginSQL = "SELECT username,role  FROM admin WHERE username='$userName' AND password='$password'";
     echo $loginSQL;
     $resultLogin = mysqli_query($conn, $loginSQL);
-    if ($resultLogin->fetch_row() > 0) {
+    if (mysqli_num_rows($resultLogin) > 0) {
         session_start();
         $_SESSION['userName'] = $userName;
+        $row = mysqli_fetch_assoc($resultLogin);
+        if ($row['role'] == '1') {
+            $_SESSION['role'] = 'superadmin';
+        }else{
+            $_SESSION['role'] = 'commonadmin';
+        }
+        // echo $_SESSION['role'];
         header("Location: index.php"); 
         exit;
     } else {
